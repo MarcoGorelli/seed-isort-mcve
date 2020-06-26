b21e656a933fdc41939926284bada169c5826caa
Marco Gorelli <m.gorelli@samsung.com>
2020-06-26 15:47:28 +0100

    a

---
+++ application_1/__main__.py
@@ -0,0 +1,5 @@
+from application_1.utils import hello
+import pandas as pd
+
+if __name__ == '__main__':
+    print(hello('alice'))
---
+++ application_2/__main__.py
@@ -0,0 +1,5 @@
+from application_2.utils import hello
+import numpy as np
+
+if __name__ == '__main__':
+    print(hello('bob'))
